0a8d2abb8ce7a15026a9af9e30f501606d6a9892
bernat alcudia <bernatalcudia@gmail.com>
2025-02-27 12:49:26 +0100

    added function queryName

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ const section = document.getElementById("section");
 const searchInput = document.getElementById("searchInput");
 const buttonSearch = document.getElementById("buttonSearch");
 
+
+let query = searchInput.value;
+
 fetch("https://dragonball-api.com/api/characters?limit=1000")
+
     .then((result) => result.json())
     .then((data) => {
         data.items.forEach(character => {
@@ -21,7 +25,6 @@ fetch("https://dragonball-api.com/api/characters?limit=1000")
 
 function cardCharacter(name, ki, maxKi, race, gender, description, image) {
 
-    console.log(name)
     //Create elements
     const charactersDiv = document.createElement("div");
     const nameH1 = document.createElement("h1");
@@ -40,9 +43,38 @@ function cardCharacter(name, ki, maxKi, race, gender, description, image) {
     genderP.innerText = gender;
     descriptionP.innerText = description;
     characterImage.setAttribute("src", image);
-
+    //Add elements to div
     charactersDiv.append(nameH1, kiP, maxKiP, raceH2, genderP, descriptionP, characterImage);
-
+    //Add element to section
     section.appendChild(charactersDiv);
 }
 
+async function queryName(event) {
+    let link = "https://dragonball-api.com/api/characters?limit=1000";
+
+    let query = event.target.value;
+    let querySearch = "&name=" + query;
+    link += querySearch;
+
+    try {
+        const result = await fetch(link);
+        let data = await result.json();
+        console.log(data);
+
+        data.forEach(character => {
+            const { name } = character;
+            const { ki } = character;
+            const { maxKi } = character;
+            const { race } = character;
+            const { gender } = character;
+            const { description } = character;
+            const { image } = character;
+
+            cardCharacter(name, ki, maxKi, race, gender, description, image);
+        });
+
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+searchInput.addEventListener("input", queryName)
